cebc2315bc791c37f496270ce72bc53af5d8d7e8
Ahmad <matt.syahab@gmail.com>
2017-11-16 19:27:30 +0700

    API Weather Done

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
+const unirest = require('unirest');
 
 app.use(bodyParser.urlencoded({ extended: false })) // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()) // parse application/json
@@ -9,10 +10,12 @@ app.use(cors())
 
 //routers
 const index = require('./routers/index')
+const weather = require('./routers/weather')
 
 
 //index
 app.use('/', index)
+app.use('/', weather)
 
 
 //listen
