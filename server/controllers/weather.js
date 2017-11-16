1630ac6a09036f5658333d8ffd7eb296aafba10d
Faris Widyantho <radenfaris@gmail.com>
2017-11-16 20:49:43 +0700

    update weather merged

--- server/controllers/weather.js
+++ server/controllers/weather.js
@@ -7,7 +7,8 @@ function checkWeather(req,res) {
 	.end(function (result) {
 		// res.send(result.status, result.headers, result.body)
 		let keepWeather = result.body.split(" ");
-		keepWeather.forEach(checkWeather => {
+		console.log(keepWeather)
+		keepWeather.forEach((checkWeather, index) => {
 			let weather = checkWeather.toLowerCase();
 			if(weather == "rain" || weather == "thunderstorms"){
 				res.send({message : "Hujan Cuy , kalo nekat pakai jas hujan cuy", weather :result.body})
@@ -15,9 +16,13 @@ function checkWeather(req,res) {
 				res.send({message : "Salju Cuy Dingin beud", weather :result.body})
 			}else if(weather == "unknown"){
 				res.send({message : "Sorry cuy Cuacanya Unknown ga Jelas", weather :result.body})
-			}								
+			}
+			console.log(index,keepWeather.length-1)
+			if(index == keepWeather.length-1){
+				res.send({message : "Cuacanya aman cuy", weather :result.body})
+			}
 		})
-		res.send({message : "Cuacanya aman cuy", weather :result.body})
+		
 	});	
 }
 
